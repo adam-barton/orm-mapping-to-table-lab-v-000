b135c6f215b9b33bf6907bd555cacf8c8e04560a
Adam Barton <adam.barton333@gmail.com>
2018-09-25 02:40:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,8 @@ class Student
     @ID = DB[conn:].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create
+  def self.create(name:, grade:)
+    student = Student.new(name:, grade:)
 
   end
 
