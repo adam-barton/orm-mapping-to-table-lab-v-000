06a9134fca111aa6f7010f882ea5f16e94d6e51b
Adam Barton <adam.barton333@gmail.com>
2018-09-25 02:44:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,26 +20,24 @@ class Student
       grade INTEGER
     );
     SQL
-    DB[conn:].execute(sql)
-
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table(table)
     sql = "DROP TABLE students"
-    DB[conn:].execute(sql)
+    DB[:conn].execute(sql)
   end
 
   def save
     sql = "INSERT INTO students (name, grade) VALUES (?, ?)"
-    DB[conn:].execute(sql, self.name, self.grade)
-    @ID = DB[conn:].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    DB[:conn].execute(sql, self.name, self.grade)
+    @ID = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   def self.create(name:, grade:)
     student = Student.new(name:, grade:)
     student.save
     student
-
   end
 
 
